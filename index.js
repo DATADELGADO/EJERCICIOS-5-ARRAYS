1b75d1bab97cffa2efe81a5255ac94c35d6b9e97
STEVEN DELGADO <delgado.steven.1997@gmail.com>
2023-03-04 10:21:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,125 @@
+//1. Declara un array _vacío_.
+
+let array = [];
+
+//2. Declara un array com mas de 5 elementos.
+
+let array1 = [1, 2, 3, 4, 5, 6, 7]
+
+//3. Encuentra la longitud de tu array.
+
+console.log(array1.length);
+
+//4. Obtenga el primer elemento, el elemento del medio y el último elemento de un array.
+
+console.log(`primer elemento: ${array1[0]}`);
+console.log(`elemento medio: ${array1[3]}`);
+console.log(`ultimo elemento: ${array1[array1.length - 1]}`);
+
+
+// 5. Declara un array llamado _mixedDataTypes_, coloque diferentes tipos de datos en el array y encuentre la longitud del array.El tamaño del array debe ser mayor que 5.
+
+let _mixedDataTypes_ = ["steven", 2, 3, false, true, [0, 2]];
+
+// 6. Declare un variable array de nombre _itCompanies_ y asignarles valores iniciales Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon.
+
+let _itCompanies_ = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+
+// 7. Imprima el array usando _console.log()_.
+
+console.log(_itCompanies_);
+
+// 8. Imprima el número de empresas en el array.
+
+console.log(_itCompanies_.length);
+
+// 9. Imprime la primer empresa, la intermedia y la última empresa
+
+console.log(`primer empresa: ${_itCompanies_[0]}`);
+console.log(`empresa medio: ${_itCompanies_[3]}`);
+console.log(`ultima empresa: ${_itCompanies_[_itCompanies_.length - 1]}`);
+
+// 10. Imprime cada empresa.
+
+for (let i = 0; i < _itCompanies_.length; i++) {
+    console.log(_itCompanies_[i]);
+}
+
+//11. Cambie el nombre de cada empresa a mayúsculas uno por uno e imprímalos.  /// revisar
+
+for (let i = 0; i < _itCompanies_.length; i++) {
+    console.log(_itCompanies_[i].toLocaleUpperCase());
+}
+
+
+//12. Imprime el array como una oración: Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon son grandes empresas de TI.
+console.log(`${_itCompanies_[0]}, ${_itCompanies_[1]}, ${_itCompanies_[2]}, ${_itCompanies_[3]}, ${_itCompanies_[4]}, ${_itCompanies_[5]} y ${_itCompanies_[6]} son grandes empresas de TI`);
+
+
+
+//13. Compruebe si existe una determinada empresa en el array itCompanies.Si existe, retorna la empresa; de lo contrario, retorna la empresa _no existe_
+let empresa = "Amazon";
+if (_itCompanies_.indexOf(empresa)) {
+    console.log(_itCompanies_[_itCompanies_.indexOf(empresa)]);
+} else {
+    console.log("La empresa no existe");
+}
+
+
+//14. Filtre las empresas que tienen más de una 'o' sin el método _filter()_
+let arrayEmpresas = [];
+for (let i = 0; i < _itCompanies_.length; i++) {
+    if (_itCompanies_[i].match(/[o]{2,}/g)) {
+        arrayEmpresas.push(_itCompanies_[i]);
+    }
+}
+console.log(arrayEmpresas);
+
+//15. Ordene el array usando el método _sort()_
+
+_itCompanies_.sort();
+console.log(_itCompanies_);
+
+//16. Invierte la array usando el método _reverse()_
+
+_itCompanies_.reverse();
+console.log(_itCompanies_);
+
+
+//17. Cortar las primeras 3 empresas del array
+
+console.log(_itCompanies_.slice(0, 3));
+
+//18. Cortar las últimas 3 empresas del array
+
+console.log(_itCompanies_.slice(_itCompanies_.length - 3, _itCompanies_.length));
+
+
+//19. Cortar la empresa o empresas intermedias de TI del array
+
+console.log(_itCompanies_.slice(_itCompanies_.length / 2, _itCompanies_.length / 2 + 1));
+
+//20. Eliminar la primera empresa de TI del array
+_itCompanies_.shift();
+console.log(_itCompanies_);
+
+
+//21. Eliminar la empresa o empresas intermedias de TI del array
+
+let _itCompanies_2 = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+_itCompanies_2.splice(3, 1);
+console.log(_itCompanies_2);
+
+//22. Elimine la última empresa de TI del array
+
+_itCompanies_2.splice(_itCompanies_2.length - 1, 1);
+console.log(_itCompanies_2);
+
+//23. Eliminar todas las empresas de TI
+
+_itCompanies_2.splice(0);
+console.log(_itCompanies_2);
+
+
+
+
---
+++ main.js
@@ -0,0 +1,104 @@
+import { countries } from "./arrays/countries.js";
+import { webTechs } from "./web_techs.js";
+
+
+// ***************************************Ejercicios: Nivel 2***************************************************
+
+
+// 1. Cree un archivo de countries.js separado y almacene el array de países en este archivo, cree un archivo separado web_techs.js y almacene el array de webTechs en este archivo.Acceda a ambos archivos en el archivo main.js
+
+
+// 2. Primero elimine todos los signos de puntuación y cambie de string a array y cuente el número de palabras en el array
+
+let text = "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+let arrayFormat = text.replaceAll(/[\.\,]/g, "").split(" ");
+console.log(arrayFormat.length);
+
+
+// 3. En el siguiente carrito de compras agregue, elimine, edite artículos
+
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+shoppingCart.unshift("Meat");
+shoppingCart.push("Sugar");
+shoppingCart.splice(shoppingCart.indexOf("Honey"), 1);
+shoppingCart[shoppingCart.indexOf("Tea")] = "Green Tea"
+console.log(shoppingCart);
+
+// 4. En el array de países, verifique si 'Ethiopia' existe en el array si existe, imprima 'ETHIOPIA'.Si no existe agregar a la lista de países.
+
+if (countries.indexOf("Ethiopia") >= 0) {
+    console.log("ETHIOPIA");
+} else {
+    countries.push("Ethiopia")
+}
+
+
+// 5. En el array webTechs, verifique si Sass existe en el array y si existe, imprima 'Sass es un preproceso de CSS'.Si no existe, agregue Sass al array e imprima el array.
+
+if (webTechs.indexOf("Sass") >= 0) {
+    console.log('Sass es un preproceso de CSS');
+} else {
+    webTechs.push("Sass");
+}
+
+
+//  6. Concatene las siguientes dos variables y guardelas en una variable fullStack.
+
+const frontEnd = ["HTML", "CSS", "JS", "React", "Redux"];
+const backEnd = ["Node", "Express", "MongoDB"];
+let fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
+
+
+
+
+
+
+
+// *********************Ejercicios: Nivel 3*********************
+
+
+// 1. El siguiente es un array de 10 edades de estudiantes:
+
+//  Ordene el array y encuentre la edad mínima y máxima
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+ages.sort();
+console.log(ages.sort());
+console.log("edad minima: " + ages[0]);
+console.log("edad maxima: " + ages[ages.length - 1]);
+
+// Encuentre la edad media(un elemento intermedio o dos elementos intermedios divididos por dos)
+
+console.log("edad media: " + ages[(ages.length) / 2]);
+
+//  Encuentre la edad promedio(todos los elementos divididos por el número de elementos)
+let promedio = (ages[0] + ages[1] + ages[2] + ages[3] + ages[4] + ages[5] + ages[6] + ages[7] + ages[8] + ages[9]) / ages.length;
+console.log(`edad promedio: ${promedio}`);
+
+// Encuentre el rango de las edades(max menos min)
+
+console.log(`el rango es ${ages[ages.length - 1] - ages[0]}`);
+
+// Compare el valor de(mínimo promedio) y(máximo  promedio), use el método _abs()_
+
+console.log(Math.round(Math.abs(ages[0] - promedio)));
+console.log(Math.round(Math.abs(ages[ages.length - 1] - promedio)));
+
+
+
+// 2. Cortar los diez primeros países de la array de países
+
+console.log(countries.slice(0, 10));
+
+// 3. Encuentre el país o países de en medio en el array de países
+console.log(countries.splice((countries.length / 2), 1));
+
+// 4. Divide el array de países en dos arrays iguales si es par. Si el array de países no es par, agregue un país más para la primera mitad.
+
+let arrayMitad = countries.splice(countries.length / 2, countries.length)
+
+console.log(countries.length);
+console.log(arrayMitad.length);
+
+
